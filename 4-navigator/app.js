bf06ff70f113112106893a2a339b965a7778e32f
Evgenij Eliseew <evgenij_eliseew@live.de>
2023-05-23 03:55:10 +0200

    Внес незначительные изменения

--- 4-navigator/app.js
+++ 4-navigator/app.js
@@ -2,8 +2,7 @@ let addressLat = 10;
 let addressLong = 12;
 let postitionLat = 34;
 let postitionLong = 25;
-let preResult = ((addressLat - addressLong) * 2) + ((postitionLat - postitionLong) * 2);
-let result = Math.sqrt(preResult);
+let result = Math.sqrt(((addressLat - addressLong) ** 2) + ((postitionLat - postitionLong) ** 2));
 
 console.log('Добро пожаловать в навигатор')
 console.log(`Расстояние между начальной и конечной точками равняется ${result} км.`);
